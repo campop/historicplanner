dec395063b1895610f276aba2729747c8abd1718
mvl22 <martin@lucas-smith.co.uk>
2018-02-17 21:05:35 +0000

    Fix sed replacement in Apache VirtualHost file

--- install.sh
+++ install.sh
@@ -175,7 +175,7 @@ chown www-data "${websiteDirectory}/enginedata/"
 # Link in Apache VirtualHost
 if [ ! -L /etc/apache2/sites-enabled/travelintimes.conf ]; then
 	ln -s $SCRIPTDIRECTORY/apache.conf /etc/apache2/sites-enabled/travelintimes.conf
-	sed -i "s/\/var\/www\/travelintimes/$softwareRoot/g" /etc/apache2/sites-enabled/travelintimes.conf
+	sed -i "s|/var/www/travelintimes|${softwareRoot}|g" /etc/apache2/sites-enabled/travelintimes.conf
 	service apache2 restart
 fi
 
