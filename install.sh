7267133bb11d8dede9cb8fca20fc230e89ce18a9
mvl22 <martin@lucas-smith.co.uk>
2018-10-01 16:30:21 +0100

    Remove an unnecessary use of sudo

--- install.sh
+++ install.sh
@@ -82,7 +82,7 @@ fi
 add-apt-repository -y ppa:openjdk-r/ppa
 apt-get update
 apt-get install -y openjdk-8-jdk
-sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 java -version
 
 # Omsosis, for pre-processing of .osm files
