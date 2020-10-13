b2eb8ecbe065edca719d6bf634cc2c56aeae70eb
mvl22 <martin@lucas-smith.co.uk>
2020-10-13 15:34:40 +0000

    Ubuntu 20.04: OSRM compile changes

--- install.sh
+++ install.sh
@@ -185,7 +185,7 @@ if [ ! -f "${osrmBackendDirectory}/build/osrm-extract" ]; then
 	apt-get install software-properties-common
 	add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	apt-get update
-	apt-get install -y build-essential cmake pkg-config libbz2-dev libstxxl-dev libstxxl1v5 libxml2-dev libzip-dev libboost-all-dev lua5.2 liblua5.2-dev libtbb-dev libluabind-dev libluabind0.9.1v5
+	apt-get install -y build-essential cmake pkg-config libbz2-dev libstxxl-dev libstxxl1v5 libxml2-dev libzip-dev libboost-all-dev lua5.2 liblua5.2-dev libtbb-dev libluabind-dev libluabind0.9.1d1
 	export CPP=cpp-6 CC=gcc-6 CXX=g++-6
 	cd $softwareRoot/
 	mkdir "$osrmBackendDirectory"
@@ -198,7 +198,7 @@ if [ ! -f "${osrmBackendDirectory}/build/osrm-extract" ]; then
 	mkdir -p build
 	chown -R travelintimes.travelintimes "${osrmBackendDirectory}/build/"
 	cd build
-	sudo -H -u travelintimes bash -c "cmake .. -DCMAKE_BUILD_TYPE=Release"
+	sudo -H -u travelintimes bash -c "cmake .. -DCMAKE_BUILD_TYPE=Release" -DCMAKE_CXX_FLAGS="-Wno-pessimizing-move -Wno-redundant-move"	# Flags added as per https://github.com/Project-OSRM/osrm-backend/issues/5797
 	sudo -H -u travelintimes bash -c "cmake --build ."
 	#cmake --build . --target install
 fi
