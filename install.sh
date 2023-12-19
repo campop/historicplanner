95eb185b9bcecf07d1d08a945300bda43566609b
mvl22 <martin@lucas-smith.co.uk>
2023-12-19 15:10:21 +0000

    Update Yarn to avoid key warning

--- install.sh
+++ install.sh
@@ -95,11 +95,11 @@ sudo apt-get update
 sudo apt-get install -y nodejs
 which node
 
-# Yarn, for JS package management; see: https://www.howtoforge.com/how-to-install-yarn-npm-package-manager-on-ubuntu-20-04/
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+# Yarn, for JS package management; see: https://www.howtoforge.com/how-to-install-yarn-npm-package-manager-on-ubuntu-20-04/ and https://stackoverflow.com/a/74123377/
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarn.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarn.gpg] https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
 apt-get update
-apt-get -y install yarn
+apt-get install -y yarn
 
 # Conversion to GeoJSON
 npm install -g osmtogeojson
